95b019d6f77beff224134d8cc09593dc1a27b9c5
babachir <ba.bachir@hotmail.fr>
2023-10-07 16:09:42 +0200

    test de metadonné

--- js/script.js
+++ js/script.js
@@ -6,11 +6,17 @@ $(document).ready(function () {
         // Convertir le nombre en entier
         numberOfImages = parseInt(numberOfImages);
 
-        // Construire les URLs des images et les ajouter au tableau
+        // Construire les données des images
         var imageFiles = [];
         for (var i = 1; i <= numberOfImages; i++) {
             var imageUrl = 'images/img' + i + '.jpg'; // Format de nom attendu
-            imageFiles.push(imageUrl);
+            getMetadata(imageUrl);
+            var imageMetadata = {
+                src: imageUrl,
+                hashtag: 'img' + i + '.jpg',
+            };
+            
+            imageFiles.push(imageMetadata);
         }
 
         // Données à utiliser avec le modèle Mustache
@@ -30,3 +36,23 @@ $(document).ready(function () {
         alert("Nombre d'images invalide. Veuillez recharger la page et entrer un nombre valide.");
     }
 });
+
+function getMetadata(imageUrl) {
+    // Charger l'image
+    var img = new Image();
+    img.src = imageUrl;
+
+    // Attendez que l'image soit chargée
+    img.onload = function () {
+        // Récupérer les métadonnées de l'image
+        EXIF.getData(img, function () {
+            var metadata = EXIF.getAllTags(this);
+            console.log(metadata); // Affiche les métadonnées dans la console
+        });
+    };
+
+    // Gérer les erreurs de chargement de l'image
+    img.onerror = function () {
+        console.error('Impossible de charger l\'image.');
+    };
+}
